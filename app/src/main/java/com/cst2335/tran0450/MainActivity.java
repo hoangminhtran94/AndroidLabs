2bc527a6dd0cab48284392c0c087f75fc074576a
Minh Hoang Tran <hoang.minhtran94@gmail.com>
2022-06-18 03:24:30 -0400

    lab03

--- app/src/main/java/com/cst2335/tran0450/MainActivity.java
+++ app/src/main/java/com/cst2335/tran0450/MainActivity.java
@@ -2,13 +2,62 @@ package com.cst2335.tran0450;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText emailView;
+    private Button loginButton;
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        emailView = (EditText) findViewById(R.id.email);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String inputEmail = emailView.getText().toString().trim();
+                Intent goToProfile = new Intent(MainActivity.this, ProfileActivity.class);
+                goToProfile.putExtra("Email",inputEmail);
+                startActivity(goToProfile);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        emailView = (EditText) findViewById(R.id.email);
+        String inputEmail = emailView.getText().toString();
+
+        preferences = getSharedPreferences("login", Context.MODE_PRIVATE);
+        String savedEmail = preferences.getString("email","");
+
+        SharedPreferences.Editor ed = preferences.edit();
+        ed.putString("email",inputEmail).apply();
+
+        if(inputEmail.isEmpty()) {
+            emailView.setText(savedEmail);
+        }
+        else {
+            emailView.setText(inputEmail);
+        }
+
+
     }
 }
